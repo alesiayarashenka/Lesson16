bf8cdd12220e35302bce43b5621f326f9fcae11d
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2025-01-31 11:47:16 +0300

    add lesson16

---
+++ src/test/java/tests/HerokuAppTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HerokuAppTest {
+    @Test
+    public void herokuLoginPageTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//*[@type = \"submit\"]")).click();
+        String expectedResult = "Welcome to the Secure Area. When you are done click logout below.";
+        String actualResult =  driver.findElement(By.xpath("//*[@class = \"subheader\"]")).getText();
+        Assert.assertEquals(expectedResult,actualResult);
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/SampleAppTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleAppTest {
+    @Test
+    public void sampleLoginPageTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://uitestingplayground.com/sampleapp");
+        driver.findElement(By.name("UserName")).sendKeys("user");
+        driver.findElement(By.name("Password")).sendKeys("pwd");
+        driver.findElement(By.xpath("//*[@id = \"login\"]")).click();
+        String expectedResult = "Welcome, user!";
+        String actualResult =  driver.findElement(By.xpath("//*[@id = \"loginstatus\"]")).getText();
+        Assert.assertEquals(expectedResult,actualResult);
+        driver.quit();
+    }
+}
